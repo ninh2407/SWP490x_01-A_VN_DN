361a3c6cea524497e48bebe2d7eb964f65473ff9
ninh2407 <85949749+ninh2407@users.noreply.github.com>
2021-09-13 21:42:37 +0700

    Update SQLLoterry.sql

--- Database/SQLLoterry.sql
+++ Database/SQLLoterry.sql
@@ -45,7 +45,62 @@ CREATE TABLE lottery_ticket
 	PRIMARY KEY (company_id, open_date, ticket_code)
 )
 
+INSERT INTO [dbo].[account]
+           ([user_mail]
+           ,[password]
+           ,[user_name]
+           ,[user_phone]
+           ,[is_admin])
+     VALUES
+           ('ninh2407@gmail.com',
+			'aabb9087',
+			'ninh',
+			'0333833908',
+			1)
+GO
 
+INSERT INTO [dbo].[company]
+           ([id]
+           ,[name]
+           ,[region])
+     VALUES
+		   ('TDo','Công ty TNHH MTV Xổ số kiến thiết Thủ Đô','MB'),
+           ('AG','Công ty TNHH MTV Xổ số kiến thiết An Giang','MN'),
+		   ('BDg','Công ty TNHH MTV Xổ số kiến thiết tỉnh Bình Dương','MN'),
+		   ('BDh','Công ty TNHH MTV Xổ số kiến thiết Bình Định','MT'),
+		   ('BL','Công ty TNHH MTV Xổ số kiến thiết Bạc Liêu','MN'),
+		   ('BPh','Công ty TNHH MTV Xổ số kiến thiết Bình Phước','MN'),
+		   ('BTr','Công ty TNHH MTV Xổ số kiến thiết Bến Tre','MN'),
+		   ('BTh','Công ty TNHH MTV Xổ số kiến thiết Bình Thuận','MN'),
+		   ('CM','Công ty TNHH MTV Xổ số kiến thiết Cà Mau','MN'),
+		   ('CTh','Công ty TNHH MTV Xổ số kiến thiết Cần Thơ','MN'),
+		   ('DLK','Công ty TNHH MTV Xổ số kiến thiết Đăk Lăk','MT'),
+		   ('DNai','Công ty TNHH MTV Xổ số kiến thiết Đồng Nai','MN'),
+		   ('DNg','Công ty TNHH MTV Xổ số kiến thiết Đà Nẵng','MT'),
+		   ('DNong','Công ty TNHH MTV Xổ số kiến thiết Đắc Nông','MT'),
+		   ('DTp','Công ty TNHH MTV Xổ số kiến thiết Đồng Tháp','MN'),
+		   ('GL','Công ty TNHH MTV Xổ số kiến thiết Gia Lai','MT'),
+		   ('HCM','Công ty TNHH MTV Xổ số kiến thiết Thành phố Hồ Chí Minh','MN'),
+		   ('HG','Công ty TNHH MTV Xổ số kiến thiết Hậu Giang','MN'),
+		   ('KG','Công ty TNHH MTV Xổ số kiến thiết Kiên Giang','MN'),
+		   ('KH','Công ty TNHH MTV Xổ số kiến thiết Khánh Hòa','MT'),
+		   ('KT','Công ty TNHH MTV Xổ số kiến thiết Kon Tum','MT'),
+		   ('LA','Công ty TNHH MTV Xổ số kiến thiết Long An','MN'),
+		   ('LD','Công ty TNHH MTV Xổ số kiến thiết Lâm Đồng','MT'),
+		   ('NT','Công ty TNHH MTV Xổ số kiến thiết Ninh Thuận','MT'),
+		   ('PY','Công ty TNHH MTV Xổ số kiến thiết Phú Yên','MT'),
+		   ('QB','Công ty TNHH MTV Xổ số kiến thiết Quảng Bình','MT'),
+		   ('QNg','Công ty TNHH MTV Xổ số kiến thiết Quảng Ngãi','MT'),
+		   ('QNam','Công ty TNHH MTV Xổ số kiến thiết Quảng Nam','MT'),
+		   ('QT','Công ty TNHH MTV Xổ số kiến thiết Quảng Trị','MT'),
+		   ('ST','Công ty TNHH MTV Xổ số kiến thiết Sóc Trăng','MN'),
+		   ('TG','Công ty TNHH MTV Xổ số kiến thiết Tiền Giang','MN'),
+		   ('TN','Công ty TNHH MTV Xổ số kiến thiết Tây Ninh','MN'),
+		   ('HUE','Công ty TNHH MTV Xổ số kiến thiết Thừa Thiên Huế','MT'),
+		   ('TV','Công ty TNHH MTV Xổ số kiến thiết Trà Vinh','MN'),
+		   ('VL','Công ty TNHH MTV Xổ số kiến thiết Vĩnh Long','MN'),
+		   ('VT','Công ty TNHH MTV Xổ số kiến thiết Vũng Tàu','MN')
+GO
 
 
 
